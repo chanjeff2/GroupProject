a8f9d429baf3b6cc8e230411564713c65e2ee27a
chanjeff2 <jeffchan142@gmail.com>
2020-11-26 01:35:06 +0800

    add modifiers to cell for safe

--- src/map/cell.h
+++ src/map/cell.h
@@ -1,20 +1,41 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include <set>
+using namespace std;
+
 // forward declaration
 class ITower;
+class IEnemy;
 class GameGrid;
 
 class Cell
 {
 	friend class GameGrid; // cell can only be created when map init
 
+	ITower *tower;
+	set<IEnemy*> enemy;
+
+	// constructor
+	Cell(int x, int y);
+	Cell(int x, int y, ITower *tower);
+
     public:
         const int x;
         const int y;
-        ITower *tower;
-        Cell(int x, int y);
-        Cell(int x, int y, ITower *tower);
+
+		// methods
+		// true -> successfully placed
+		bool placeTower(ITower *tower);
+		void removeTower();
+
+		void addEnemy(IEnemy *enemy);
+		void removeEnemy(IEnemy *enemy);
+
+		// no enemy nor tower
+		void isEmpty();
+
+		void haveTower();
 };
 
 #endif // CELL_H
