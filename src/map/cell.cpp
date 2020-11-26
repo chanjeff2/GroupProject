d36e72155d5fe058bab642196fa3c6b2fc26d000
chanjeff2 <jeffchan142@gmail.com>
2020-11-27 00:12:57 +0800

    add getter and setter to cell

--- src/map/cell.cpp
+++ src/map/cell.cpp
@@ -5,3 +5,49 @@
 Cell::Cell(int x, int y): x(x), y(y), tower(nullptr) {};
 
 Cell::Cell(int x, int y, ITower *tower): x(x), y(y), tower(tower) {};
+
+// getter
+ITower *Cell::getTower() const {
+	return this->tower;
+}
+
+set<IEnemy*> Cell::getEnemy() const {
+	return this->enemy;
+}
+
+// methods
+// true -> successfully placed
+bool Cell::placeTower(ITower *tower) {
+	if (this->tower != nullptr) {
+		return false;
+	} else {
+		this->tower = tower;
+		return true;
+	}
+}
+
+void Cell::removeTower() {
+	delete this->tower;
+	this->tower = nullptr;
+}
+
+void Cell::addEnemy(IEnemy *enemy) {
+	this->enemy.insert(enemy);
+}
+
+void Cell::removeEnemy(IEnemy *enemy) {
+	this->enemy.erase(enemy);
+}
+
+// no enemy nor tower
+bool Cell::isEmpty() const {
+	if (this->tower != nullptr || !enemy.empty()) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+bool Cell::hasTower() const {
+	return this->tower != nullptr;
+}
