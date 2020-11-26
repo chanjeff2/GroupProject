a1a2a34682878095b832b71245dfa69eaff7ef1a
CurlyRhapsody <matthewchan60792@gmail.com>
2020-11-26 17:41:49 +0800

    Managers Redux Part 1

--- src/utility/WeekManager.h
+++ src/utility/WeekManager.h
@@ -10,7 +10,7 @@ class WeekManager: public QObject {
 	int week; // wave
 	bool isWeekCooldown; // true -> ready to start next week
 	std::atomic_int skippedWeeks; // no. of weeks skipped ** atomic for thread safe
-    Qtimer* timer;
+    QTimer* timer;
     WeekLayoutManager* weekLayoutManager;
 
 	void goToNextWeek();
