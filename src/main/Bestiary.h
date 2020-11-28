836d9d9459aa54a9005f6c6d5605c39d29015dd5
CurlyRhapsody <matthewchan60792@gmail.com>
2020-11-28 20:29:34 +0800

    Added Bestiary Window, Part 2

--- src/main/Bestiary.h
+++ src/main/Bestiary.h
@@ -1,4 +1,53 @@
 #ifndef BESTIARY_H
 #define BESTIARY_H
 
+#include <QWidget>
+#include <QGraphicsView>
+#include <QTextBrowser>
+
+class BestiaryWindow : public QWidget {
+    Q_OBJECT
+public:
+    BestiaryWindow(QWidget *parent = nullptr);
+    ~BestiaryWindow();
+
+private slots:
+
+    // Towers
+    void on_Regular_clicked();
+    void on_Arts_clicked();
+    void on_Wolfram_clicked();
+    void on_Hacker_clicked();
+    void on_Calculator_clicked();
+    void on_Nerd_clicked();
+    void on_Ghostwriter_clicked();
+    void on_Chegg_clicked();
+
+    // Enemies
+    void on_NormalHW_clicked();
+    void on_Essay_clicked();
+    void on_EncryptedHW_clicked();
+    void on_GroupProj_clicked();
+    void on_Webwork_clicked();
+    void on_Classviva_clicked();
+
+    void on_MathPaperwork_clicked();
+    void on_CppFile_clicked();
+    void on_HFile_clicked();
+    void on_PA_clicked();
+    void on_Desmond_clicked();
+    void on_PaperPopQuiz_clicked();
+
+    void on_IPRS_clicked();
+    void on_Midterm_clicked();
+    void on_MathExam_clicked();
+    void on_FINAL_clicked();
+    void on_ELPA_clicked();
+    void on_FYP_clicked();
+
+    // Exit Bestiary
+    void on_ExitBestiary_clicked();
+
+};
+
 #endif // BESTIARY_H
