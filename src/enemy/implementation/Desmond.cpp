952282c610c3c87a66f5276f774c0d95c347cad1
Imperator4096 <66269796+Imperator4096@users.noreply.github.com>
2020-12-04 12:23:01 +0800

    fixed desmond hp, nerfed for testing

--- src/enemy/implementation/Desmond.cpp
+++ src/enemy/implementation/Desmond.cpp
@@ -5,7 +5,7 @@
 
 Desmond::Desmond(EnemyUtility *enemyUtility, Path path): IEnemy(enemyUtility, path, EnemyType::Desmond)
 {
-    HP = 100;
+    HP = 1000;
     speed = 2.4;
     worth = 9;
 	armor = 6;
